2fdc5982253e8d6b41dcdba10bcdb671263eaf2f
Angela <lika.ponomarenko@mail.ru>
2023-11-02 16:11:04 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+//1
+
+// const a = [1, 2, 3, 4, 5]
+// let result = 0
+//
+//     for (let i = 0; i <= a.length; i++) {
+//         if (typeof a[i] === 'number') {
+//             result += a[i]
+//         }
+//     }
+//
+// console.log(result)
+
+//2
+
+// const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let result = 0
+//
+//     for (let i = 0; i <= a.length; i++) {
+//         if (typeof a[i] === 'number') {
+//             result += a[i]
+//         }
+//     }
+//
+// console.log(result)
+
+//3
+
+// const a = [1, 2, 5, 9, 4, 13, 4, 10]
+
+// function myF() {
+//     for ( let i = 0; i <= a.length; i++) {
+//         if (a.map(el => el === 4)) {
+//             return 'correct'
+//         } else {
+//             return 'not correct'
+//         }
+//     }
+// }
+//
+// console.log(myF())
+
+
